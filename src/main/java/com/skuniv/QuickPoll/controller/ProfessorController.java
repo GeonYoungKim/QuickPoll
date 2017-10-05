2f97d7c3cd19de472d6107664a2a9ae16240be1d
DESKTOP-TCNU2B4\moonsu <visitant335@gmail.com>
2017-10-06 00:54:48 +0900

    apply to objective question.

--- src/main/java/com/skuniv/QuickPoll/controller/ProfessorController.java
+++ src/main/java/com/skuniv/QuickPoll/controller/ProfessorController.java
@@ -3,13 +3,28 @@ package com.skuniv.QuickPoll.controller;
 import java.util.List;
 import java.util.Map;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.skuniv.QuickPoll.service.ProfessorService;
+
 @Controller
 public class ProfessorController {
+
+	@Resource(name = "ProfessorService")
+	private ProfessorService professorService;
 	
+	@RequestMapping(value = "/resultListForObjectiveQuestion")
+	public ModelAndView studentList(HttpServletRequest request) throws Exception {
+		int quickpoll_question_id = Integer.parseInt(request.getParameter("quickpoll_question_id"));
+		System.out.println("quickpoll_question_id : " + quickpoll_question_id);
+		ModelAndView mv = new ModelAndView("jsonView");
+		Map<String, Object> resultList = professorService.selectResultListForObjectiveQuestion(quickpoll_question_id);
+		mv.addObject("resultListForObjectiveQuestion", resultList);
+		return mv;
+	}
 }
