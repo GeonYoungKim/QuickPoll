213fe055a719fc0a0b6a728f66ed7325137721f1
MOJUNSEO <mojunseo49@naver.com>
2017-11-21 19:01:44 +0900

    first commit

--- src/main/java/com/skuniv/QuickPoll/connect/ConnectDB.java
+++ src/main/java/com/skuniv/QuickPoll/connect/ConnectDB.java
@@ -4,8 +4,9 @@ import java.util.List;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-
+//junseo
 public class ConnectDB {
+	
 	@Autowired
 	private SqlSessionTemplate sqlSession; // connect DB	
 	
